a5689a1b027760b7b3aec267840e241f509f4129
Mia Peeples <peeples@princeton.edu>
2023-11-14 16:06:10 -0500

    Fix: Reformat to pass pre-commit

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,4 +1,5 @@
 from reactive_transport import simple_transport
 
+
 def test_simple():
-    assert simple_transport(1,1,1,1) == simple_transport(1,1,1,1)
+    assert simple_transport(1, 1, 1, 1) == simple_transport(1, 1, 1, 1)
