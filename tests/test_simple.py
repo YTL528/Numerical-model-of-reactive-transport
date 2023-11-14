147187624c6dbca87492e3e2cde5fd4a63a2c5e3
Mia Peeples <peeples@princeton.edu>
2023-11-14 16:21:47 -0500

    Fix: __init__.py and Change: test_simple.py

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,4 +2,4 @@ from reactive_transport import simple_transport
 
 
 def test_simple():
-    assert simple_transport(1, 1, 1, 1) == simple_transport(1, 1, 1, 1)
+    assert simple_transport(1, 1, 1, 1) == -0.1875
