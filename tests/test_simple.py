3f83a89e5328175c8a6aff1824bbfa07efba8cb4
Mia Peeples <peeples@princeton.edu>
2023-11-17 18:00:40 -0500

    Add: Additional tests in test_simpl e.pya

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -3,3 +3,11 @@ from reactive_transport import simple_transport
 
 def test_simple():
     assert simple_transport(0.25, 1, 1, 0.25) == -0.1875
+
+
+def test_zeros():
+    assert simple_transport(0, 0, 0, 0) == 0
+
+
+def test_conpare():
+    assert simple_transport(0.25, 1, 1, 0.25) > simple_transport(0.5, 1, 1, 0.25)
