4fe93fc0489eafec8f723d78f8370219e85ece21
Mia Peeples <peeples@princeton.edu>
2023-11-20 10:32:11 -0500

    Ran pre-commit

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -13,12 +13,11 @@ def test_compare():
     assert abs(simple_transport(0.25, 1, 1, 0.25)) > abs(
         simple_transport(0.5, 1, 1, 0.25)
     )
-    
-    
+
+
 def test_boundary_conditions():
     assert simple_transport(1000, 1e-6, 10, 100) == -999
 
 
 def test_negative_inputs():
     assert simple_transport(-0.25, 1, -1, -0.25) == -0.1875
-    
