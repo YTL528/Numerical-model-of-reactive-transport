052ef08db2b5074c08c36ea62cb86f4051bca51b
miapeeples <138705330+miapeeples@users.noreply.github.com>
2023-11-20 10:12:31 -0500

    Update tests/test_simple.py

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -16,4 +16,4 @@ def test_boundary_conditions():
     assert simple_transport(1000, 1e-6, 10, 100) == -999
 
 def test_negative_inputs():
-    assert simple_transport(-0.25, 1, -1, -0.25) == 0.3125
+    assert simple_transport(-0.25, 1, -1, -0.25) == -0.1875
