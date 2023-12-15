b27c0e856bcf572b396ec21d5eb5a2bcb00db1ea
Yitian <84977645+YTL528@users.noreply.github.com>
2023-12-14 21:12:41 -0500

    Fix bugs in test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ from reactive_transport import Analytical, Crank_Nicolson, Forward_Difference
 
 
 def test_boundary_values():
-    # Test boundary values for Crank_Nicolson function
+    # Test boundary values for the Crank_Nicolson function
     cn_result = Crank_Nicolson(
         L=100.0, DX=1.0, Tmax=10.0, por=0.4, DT=0.1, Cf=1.0, Cb=0.5, q=0.8, DH=0.2
     )
@@ -28,37 +28,35 @@ def test_boundary_values():
 def test_values_within_range():
     # Test if calculated values are within the range of Cf and Cb for Crank_Nicolson
     cn_result = Crank_Nicolson(
-        L=100.0, DX=1.0, Tmax=10.0, por=0.4, DT=0.1, Cf=1.0, Cb=0.5, q=0.8, DH=0.2
+        L=100.0, DX=1.0, Tmax=5.0, por=0.3, DT=0.05, Cf=100.0, Cb=0.0, q=1.5, DH=2.5
     )[:, -1]
-    assert np.all(np.logical_and(cn_result >= 0.5, cn_result <= 1.0))
+    assert np.all(np.logical_and(cn_result >= 0.0, cn_result <= 100.0))
 
     # Test if calculated values are within the range of Cf and Cb for Analytical
     ana_result = Analytical(
-        L=100.0, DX=1.0, Tmax=10.0, por=0.4, DT=0.1, Cf=1.0, Cb=0.5, q=0.8, DH=0.2
+        L=100.0, DX=1.0, Tmax=5.0, por=0.3, DT=0.05, Cf=100.0, Cb=0.0, q=1.5, DH=2.5
     )
-    assert np.all(np.logical_and(ana_result >= 0.5, ana_result <= 1.0))
+    assert np.all(np.logical_and(ana_result >= 0.0, ana_result <= 100.0))
 
     # Test if calculated values are within the range of Cf and Cb for Forward_Difference
     fd_result = Forward_Difference(
-        L=100.0, DX=1.0, Tmax=10.0, por=0.4, DT=0.1, Cf=1.0, Cb=0.5, q=0.8, DH=0.2
+        L=100.0, DX=1.0, Tmax=5.0, por=0.3, DT=0.05, Cf=100.0, Cb=0.0, q=1.5, DH=2.5
     )[:, -1]
-    assert np.all(np.logical_and(fd_result >= 0.5, fd_result <= 1.0))
+    assert np.all(np.logical_and(fd_result >= 0.0, fd_result <= 100.0))
 
 
 def test_extreme_cases():
-    # Test extreme values for Crank_Nicolson function when Cf = Cb = c
-    constant_c = 0.7  # can change this value to any non-negative constant
-
+    # Test extreme values for Crank_Nicolson function when Cf = Cb = 0
     cn_result = Crank_Nicolson(
         L=100.0,
         DX=1.0,
         Tmax=10.0,
-        por=0.4,
-        DT=0.1,
-        Cf=constant_c,
-        Cb=constant_c,
-        q=0.8,
-        DH=0.2,
+        por=0.3,
+        DT=0.05,
+        Cf=0,
+        Cb=0,
+        q=1.5,
+        DH=2.5,
     )[:, -1]
     assert np.allclose(cn_result, constant_c)
 
@@ -67,12 +65,12 @@ def test_extreme_cases():
         L=100.0,
         DX=1.0,
         Tmax=10.0,
-        por=0.4,
-        DT=0.1,
-        Cf=constant_c,
-        Cb=constant_c,
-        q=0.8,
-        DH=0.2,
+        por=0.3,
+        DT=0.05,
+        Cf=0,
+        Cb=0,
+        q=1.5,
+        DH=2.5
     )
     assert np.allclose(ana_result, constant_c)
 
@@ -81,26 +79,26 @@ def test_extreme_cases():
         L=100.0,
         DX=1.0,
         Tmax=10.0,
-        por=0.4,
-        DT=0.1,
-        Cf=constant_c,
-        Cb=constant_c,
-        q=0.8,
-        DH=0.2,
+        por=0.3,
+        DT=0.05,
+        Cf=0,
+        Cb=0,
+        q=1.5,
+        DH=2.5
     )[:, -1]
     assert np.allclose(fd_result, constant_c)
 
 
 def test_close_results():
-    # Test that all three functions return same size and approx. value for same inputs
+    # Test that all three functions return the same size and approx. value for same inputs
     cn_result = Crank_Nicolson(
-        L=100.0, DX=1.0, Tmax=10.0, por=0.4, DT=0.1, Cf=1.0, Cb=0.5, q=0.8, DH=0.2
+        L=100.0, DX=1.0, Tmax=5.0, por=0.3, DT=0.05, Cf=100.0, Cb=0.0, q=1.5, DH=2.5
     )[:, -1]
     ana_result = Analytical(
-        L=100.0, DX=1.0, Tmax=10.0, por=0.4, DT=0.1, Cf=1.0, Cb=0.5, q=0.8, DH=0.2
+        L=100.0, DX=1.0, Tmax=5.0, por=0.3, DT=0.05, Cf=100.0, Cb=0.0, q=1.5, DH=2.5
     )
     fd_result = Forward_Difference(
-        L=100.0, DX=1.0, Tmax=10.0, por=0.4, DT=0.1, Cf=1.0, Cb=0.5, q=0.8, DH=0.2
+        L=100.0, DX=1.0, Tmax=5.0, por=0.3, DT=0.05, Cf=100.0, Cb=0.0, q=1.5, DH=2.5
     )[:, -1]
 
     assert len(cn_result) == len(ana_result) == len(fd_result)
